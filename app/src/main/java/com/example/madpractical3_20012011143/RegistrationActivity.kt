e42d01b3daa739938873f357d9f7be5f7398d0b4
saahil09 <sonupurohit172002@gmail.com>
2022-11-20 14:39:31 +0530

    first commit

---
+++ app/src/main/java/com/example/madpractical3_20012011143/RegistrationActivity.kt
@@ -0,0 +1,28 @@
+package com.example.madpractical3_20012011143
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madpractical3_20012011143.databinding.ActivityRegistrationBinding
+
+class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegistrationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_nav_reg
+        binding.bottomNavigationView.setOnItemSelectedListener { it1 ->
+            when(it1.itemId){
+                R.id.bottom_nav_login -> {
+                    Intent(this,LoginActivity::class.java).also{startActivity(it)}
+                }
+
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+}
